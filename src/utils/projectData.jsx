7a82ed1200bc89e7b6beb28e5f91338482c7ed95
Yashpreet Singh <singhyashpreet22@gmail.com>
2026-03-14 16:20:03 +0530

    Added new projects to projectData array

--- src/utils/projectData.jsx
+++ src/utils/projectData.jsx
@@ -3,6 +3,12 @@ import spandan from "../assets/projectImg/spandan.png";
 import blacktree from "../assets/projectImg/blacktree.png"
 import xpertpv from "../assets/projectImg/xpertpv.png"
 import freshguard from "../assets/projectImg/freshguard.png"
+import aniocean from "../assets/projectImg/aniocean.png"
+import fitness from "../assets/projectImg/fitness.png"
+import fitsip from "../assets/projectImg/fitsip.png"
+import sp3 from "../assets/projectImg/sp3.0.png"
+import tnp from "../assets/projectImg/tnp.png"
+import oak7 from "../assets/projectImg/oak7.png"
 
 export const projectData = [
     {
@@ -29,16 +35,64 @@ export const projectData = [
         hasDevelopment: false,
         link: "https://www.figma.com/design/TbNDpPbSWuCs43MWuxjPgo/Blacktree?node-id=320-2738&t=PCqi3KOEs0ueAQR7-1"
     },
-    {
+     {
         id: 4,
+        title: "FitSip",
+        imgSrc: fitsip,
+        year: "2025",
+        hasDevelopment: true,
+        link: "https://www.fitsip.in"
+    },
+     {
+        id: 5,
+        title: "HOTEL OAK7",
+        imgSrc: oak7,
+        year: "2025",
+        hasDevelopment: true,
+        link: "http://hotel7-oaks.vercel.app/"
+    },
+     {
+        id: 6,
+        title: "Evolve Fitness",
+        imgSrc: fitness,
+        year: "2025",
+        hasDevelopment: false,
+        link: "https://www.figma.com/design/6O9t6eYuDIf4m86z0Yduy7/Evolve-Fitness?node-id=0-1&t=KSgwfWR3cWD9S0ly-1"
+    },
+     {
+        id: 7,
+        title: "Spandan 3.0",
+        imgSrc: sp3,
+        year: "2025",
+        hasDevelopment: false,
+        link: "https://www.figma.com/design/VVbEKO1WWG1RLKjcOZlFmO/Spandan-3.0?node-id=0-1&t=15DcEhSOSIkc0hPK-1"
+    },
+     {
+        id: 8,
         title: "XpertPV",
         imgSrc: xpertpv,
         year: "2024",
         hasDevelopment: false,
         link: "https://www.figma.com/design/7rrjzoxCK36WHVhOQHzVPV/XpertPV?node-id=1-2&t=6NTHVdT4XDfe3IY6-1"
     },
+     {
+        id: 9,
+        title: "TnP Portal",
+        imgSrc: tnp,
+        year: "2025",
+        hasDevelopment: true,
+        link: "https://gbpiet-desk.vercel.app/1"
+    },
     {
-        id: 5,
+        id: 10,
+        title: "AniOcean",
+        imgSrc: aniocean,
+        year: "2025",
+        hasDevelopment: true,
+        link: "https://ani-ocean.vercel.app/"
+    },
+    {
+        id: 11,
         title: "Freshguard",
         imgSrc: freshguard,
         year: "2023",
